10578960b3377827a5fbb96996adcd3e00d0a52d
eliav2 <louski.a@gmail.com>
2022-11-24 05:21:22 +0200

    initial-commit

---
+++ run.py
@@ -0,0 +1,8 @@
+import pyodide_js
+print(f"Pyodide version {pyodide_js.version}")
+import sys
+print(f"Python version {sys.version}")
+import os
+print("eliav")
+import json
+print(json.dumps({"test":"val"}))
